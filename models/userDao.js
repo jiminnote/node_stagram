cf08879eebd427d16768dd0eab42d616d4510d66
𝙹𝚒 𝙼𝚒𝚗 <jimny@ui-MacBookPro.local>
2022-08-16 00:12:16 +0900

    CREATE : signup

--- models/userDao.js
+++ models/userDao.js
@@ -3,16 +3,17 @@ const { myDataSource } = require('../config/mysql');
 const signup = async (name, email, nickname, password) => {
 	try {
 		return await myDataSource.query(
-		`INSERT INTO users_table(
+		`INSERT INTO users(
 			name,
 			email,
 			nickname,
-			password,
+			password
 		) VALUES (?, ?, ?, ?); 
 		`,
 		[name, email, nickname, password]
 	  );} 
 	  	catch (err) {
+        console.log(err)
 		const error = new Error('INVALID_DATA_INPUT');
 		error.statusCode = 500;
 		throw error;
